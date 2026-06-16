c1fadacd323dc43e631dade6f7b275f23b8b3e62
hr951 <hiroki.saki.951@gmail.com>
2026-06-17 00:36:03 +0900

    表記ゆれの修正

--- interactions/modals/winRate_submit.js
+++ interactions/modals/winRate_submit.js
@@ -12,9 +12,9 @@ module.exports = {
             { name: "目標の勝率", value: `${winRate.toFixed(2)} %` },
             { name: "必要な勝利数", value: `${((winRateBy100 * loses) / (10000 - winRateBy100)).toFixed(0)} 勝` },
             { name: "残り勝利数", value: `${wins ? ((winRateBy100 * loses) / (10000 - winRateBy100)).toFixed(0) - wins : "Undefined"} 勝` },
-            { name: "現在の勝利数", value: wins ? wins : "Undefined" },
-            { name: "現在の敗北数", value: loses },
-            { name: "現在の勝率", value: wins ? ((Number(wins) / (Number(wins) + Number(loses))) * 100).toFixed(2) : "Undefined" },
+            { name: "現在の勝利数", value: `${wins ? wins : "Undefined"} 勝` },
+            { name: "現在の敗北数", value: `${loses} 敗` },
+            { name: "現在の勝率", value: `${wins ? ((Number(wins) / (Number(wins) + Number(loses))) * 100).toFixed(2) : "Undefined"} %` },
         ];
 
         const embed = fields_embed("勝率計算", "目標の勝率から必要な勝利数を計算しました！", fields);
